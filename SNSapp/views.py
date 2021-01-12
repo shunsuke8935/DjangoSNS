aba96ec94bafee28ffcc12a0571ed6ca4b43c8fa
gotoshunsuke <shunsuke8935@gmail.com>
2021-01-12 19:55:24 +0900

    　Login develop

--- SNSapp/views.py
+++ SNSapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        save_check = User.objects.filter(username=username).first()
+        if save_check:
+            return render(request, 'signup.html', {'error':'このユーザーは登録されています。'})
+        else:
+            user = User.objects.create_user(username, '', password)
+            print(request.POST)
+            return render(request, 'signup.html', {'some':100})
+    return render(request, 'signup.html', {'some':100})
